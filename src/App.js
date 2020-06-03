591b3dfbe92b4678ec82c469223ca6dd73872d26
Xavier Jodoin <xavier.jodoin@greybox.biz>
2020-06-03 15:22:48 -0400

    Base test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+// import { Typography, Popover, Button } from "@material-ui/core";
+import logo from "./logo.svg";
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <div className="Regles">
+          À l'aide de React et Material-ui, construire un{" "}
+          <a href="https://material-ui.com/components/popover/" className="lien">
+            Popover
+          </a>{" "}
+          flexible et réutilisable
+        </div>
+        <ul className="rules">
+          <li>
+            Libre à vous du look final, mais un exemple de ce qui est attendu se trouve dans
+            /public/exemple.mp4
+          </li>
+          <li>Le code doit être clair et de vous</li>
+          <li>Le data qui doit être contenu dans le popover est dans la variable DATA</li>
+          <li>L'ajout de tests est fortement conseillé</li>
+        </ul>
       </header>
     </div>
   );
 }
+
+//uncomment this 
+// const DATA = [
+//   {
+//     question:
+//       "Aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit1  ?",
+//     answer: "Yes",
+//   },
+//   {
+//     question:
+//       "Lore aut odit aut fugit, sed quia ceos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum qui dolor sit ?",
+//     answer: "Yes",
+//   },
+//   {
+//     question: "Lorem ipsum dolor sit amet, sum dolor sit amet, consectetuer ?",
+//     answer: "No",
+//   },
+//   {
+//     question:
+//       "Bem ipsum dolor sit amet, consectetuer, sum dolor sit amet, lorem ipsum dolor sit amet, consectetuer ?",
+//     answer: "Yes",
+//   },
+// ];
+
 
 export default App;
